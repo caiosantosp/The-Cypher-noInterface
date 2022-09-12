287a062e2b30a185e9d7cdc71e649e79ad9081d1
Caio Portugal <caiosantos.portugal@hotmail.com>
2022-09-12 11:10:20 -0300

    first commit

---
+++ TheCypher01/Criptografar.cs
@@ -0,0 +1,22 @@
+﻿namespace TheCypher01
+{
+    public class Criptografar : Dicionario
+    {
+        public static string criptografarTexto(string texto)
+        {
+            texto = texto.ToLower();
+            for (int i = 0; i < texto.Length; i++)
+            {
+                foreach (KeyValuePair<string, string> letras in dicionarios)
+                {
+                    if (texto.Substring(i, 1).Equals(letras.Key))
+                    {
+                        texto = texto.Replace(texto.Substring(i, 1), dicionarios[texto.Substring(i, 1)]);
+                    }
+                }
+            }
+
+            return texto;
+        }
+    }
+}
---
+++ TheCypher01/Descriptografar.cs
@@ -0,0 +1,23 @@
+﻿namespace TheCypher01
+{
+    public class Descriptografar : Dicionario
+    {
+        public static string descriptografarTexto(string texto)
+        {
+            texto = texto.ToLower();
+            for (int i = 0; i < texto.Length; i++)
+            {
+                foreach (KeyValuePair<string, string> letras in dicionarios)
+                {
+                    if (texto.Contains(letras.Value))
+                    {
+                        texto = texto.Replace(letras.Value, letras.Key);
+                    }
+                }
+            }
+
+            return texto;
+        }
+
+    }
+}
